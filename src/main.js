6c40799be5e9631aa86fa9e5fbf858a3a831641f
ryanbas21 <ryanbas21@gmail.com>
2017-04-08 16:18:09 -0400

    saving some strikeout styleing on click and added some directive if elses

--- src/main.js
+++ src/main.js
@@ -8,15 +8,25 @@ new Vue({
   el: '#app',
   template: `
     <div id="app">
-        <h5 style="display: inline" v-if='newtodo'>Add
-        <pre style="display: inline">{{newtodo}}</pre> to the todo list?</h5></a>
+      <h5 style="display: inline" v-if='newtodo'>Add
+        <pre style="display: inline">{{newtodo}}</pre> to the todo list?
+      </h5>
+      <h5 style="display: inline" v-else>Add an Item below</h5>
         <br />
       <input style='outline:none' type='text' v-model='newtodo' @keyup.enter="addTodo"/>
-        <input style='outline:none' type='submit' value='Add Todo' />
+      <input style='outline:none' type='submit' value='Add Todo' v-on:click="addTodo"/>
       <ul>
-        <li style="list-style:none" v-for='(todo, index) in todos' on:click="">
-          {{ todo.value }}
-          <input type='submit' value='delete' v-on:click='todos.splice(index, 1)'/></li>
+        <li style="list-style:none"
+            v-for='(todo, index) in todos'
+            v-on:click="() => toggleTodo(todo)"
+        >
+            <span v-if='todo.completed == true' style='text-decoration:line-through;'>{{ todo.value }}</span>
+            <span v-else style='text-decoration:none;'>{{ todo.value }}</span>
+          <input
+            type='submit'
+            value='delete'
+            v-on:click='todos.splice(index, 1)'/>
+        </li>
       </ul>
     </div>`,
   data: {
@@ -29,8 +39,14 @@ new Vue({
       if (!this.newtodo) return;
       this.todos.push({
         value: this.newtodo,
+        completed: false,
       });
       this.newtodo = '';
+    },
+    toggleTodo(todo) {
+      const val = todo;
+      if (todo.completed) val.completed = false;
+      else val.completed = true;
     },
   },
 });
